c6411347febb2218d4027aad45054e6399a2a0f1
sergey-grebenshchikov <sergey.grebenshchikov@codecentric.de>
2017-01-13 12:55:56 +0100

    Modified common_install_redhat

--- scripts/common_install_redhat.sh
+++ scripts/common_install_redhat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+sudo yum upgrade --assumeyes --tolerant
+sudo yum update --assumeyes
 sudo yum -y update
 sudo tee /etc/yum.repos.d/docker.repo <<-EOF
 [dockerrepo]
@@ -10,5 +12,22 @@ gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg
 EOF
 
-sudo yum install -y docker-engine
+sudo tee /etc/modules-load.d/overlay.conf <<-EOF
+overlay
+EOF
+sudo modprobe overlay
+sudo mkdir -p /etc/systemd/system/docker.service.d && sudo tee /etc/systemd/system/docker.service.d/override.conf <<-EOF
+[Service]
+ExecStart=
+ExecStart=/usr/bin/docker daemon --storage-driver=overlay
+EOF
+sudo chmod -w /etc/systemd/system/docker.service.d/override.conf
+
+sudo yum install -y docker-engine-1.11.2
+sudo yum install -y docker-engine-selinux-1.11.2
+sudo yum install -y chrony
+sudo yum install -y java-1.8.0-openjdk.x86_64
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+sudo python get-pip.py
+sudo pip install virtualenv
 sudo service docker start
